af7589bd047b9108ed4c4015e1266919f6548910
Furqon Lanang <furqon.lanang24056@mhs.uingusdur.ac.id>
2026-09-22 12:59:32 +0700

    feat: tambah SafeAreaView dan responsive layout

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,29 +1,33 @@
-// app/index.tsx
+// src/app/(tabs)/index.tsx
 import { useEffect, useState } from "react";
-import { View } from "react-native";
-// Catatan: gunakan ../components/ jika file berada di dalam folder app/
-// yang sejajar dengan folder components/
+import { useWindowDimensions } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import RiwayatList from "../../../components/RiwayatList";
 import SearchBox from "../../../components/SearchBox";
 import WeatherCard from "../../../components/WeatherCard";
+
 export default function HalamanUtama() {
   const [kotaAktif, setKotaAktif] = useState("Pekalongan");
   const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
-  // Tambahkan useEffect untuk mencatat perubahan kota aktif
+  const { width } = useWindowDimensions();
+  const isTablet = width > 768;
+
   useEffect(() => {
     console.log("Kota aktif berubah menjadi:", kotaAktif);
   }, [kotaAktif]);
+
   function handleCari(kota: string) {
     setKotaAktif(kota);
     if (!riwayat.includes(kota)) {
       setRiwayat([...riwayat, kota]);
     }
   }
+
   return (
-    <View style={{ padding: 16, paddingTop: 50, gap: 16 }}>
+    <SafeAreaView style={{ flex: 1, padding: isTablet ? 32 : 16, gap: 16 }}>
       <SearchBox onCari={handleCari} />
       <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
       <RiwayatList daftarKota={riwayat} />
-    </View>
+    </SafeAreaView>
   );
 }
